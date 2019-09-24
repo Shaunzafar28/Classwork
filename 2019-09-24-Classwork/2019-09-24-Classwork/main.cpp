72079cebd5f795d04b201d82a4f7495dcd18077a
Shaunzafar28 <55443382+Shaunzafar28@users.noreply.github.com>
2019-09-24 08:41:21 -0400

    Add files via upload

---
+++ 2019-09-24-Classwork/2019-09-24-Classwork/main.cpp
@@ -0,0 +1,31 @@
+// Shaun Zafar
+// Intro to CS
+// Mr.Williams
+//Period1
+// 2019-09-24-Classwork
+
+#include<iostream>
+
+int main()
+{
+	int x = 7;
+	int y = 28;
+	float b = 90;
+	int p = 29;
+	int o = 50;
+	int w = 42;
+	int i = 39;
+	int l = 70;
+	int a = 0;
+	int m = 61;
+	std::cout << x << std::endl;
+	std::cout << y << std::endl;
+	std::cout << b << std::endl;
+	std::cout << p << std::endl;
+	std::cout << o << std::endl;
+	std::cout << w << std::endl;
+	std::cout << i << std::endl;
+	std::cout << l << std::endl;
+	std::cout << a << std::endl;
+	std::cout << m << std::endl;
+}
