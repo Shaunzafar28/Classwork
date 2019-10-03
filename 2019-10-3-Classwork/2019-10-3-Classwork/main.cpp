ef4306d7f4aa9e91c51e867ff77e24142bdef82c
Shaunzafar28 <55443382+Shaunzafar28@users.noreply.github.com>
2019-10-03 08:38:54 -0400

    Add files via upload

---
+++ 2019-10-3-Classwork/2019-10-3-Classwork/main.cpp
@@ -0,0 +1,16 @@
+// Shaun Zafar
+// Intro to CS
+// Period 1
+// Mr. Williams
+// 2019-10-03-Classwork
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	std::cout << "Enter your favorite number:";
+	int favoriteNumber;
+	std::cin >> favoriteNumber;
+	std::cout << "cool," << "is my favorite number too!" << std::endl;
+}
